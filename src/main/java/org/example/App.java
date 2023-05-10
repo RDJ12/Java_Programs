2aaa947e337997c295c41a16590d3c10a4383eee
romy john <romyjohn12.rj@gmail.com>
2023-05-10 22:51:59 +0530

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.controller.Comparable;
+import org.example.controller.Comparators;
+import org.example.controller.Serialisable;
+
+public class App {
+    public static void main(String[] args) {
+
+//        Serialisable serialisable = new Serialisable();
+//        serialisable.start();
+//
+        Comparable comparable = new Comparable();
+        comparable.start();
+//
+//        Comparators comparators=new Comparators();
+//        comparators.start();
+
+    }
+}
